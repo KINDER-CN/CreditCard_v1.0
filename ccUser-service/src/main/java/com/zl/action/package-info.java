42ac6e2466c66cc536f9fe0eb6509c8a5259dfc8
Kinder <549662562@qq.com>
2019-08-13 20:23:01 +0800

    用户模块服务

---
+++ ccUser-service/src/main/java/com/zl/action/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * 控制层
+ * 返回json格式
+ */
+/**
+ * @author KINDER
+ *
+ */
+package com.zl.action;
---
+++ ccUser-service/src/main/java/com/zl/api/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 编写调用其它服务得API
+ */
+/**
+ * @author KINDER
+ *
+ */
+package com.zl.api;
